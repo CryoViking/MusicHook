c1e108a546529a4289e49febb5657011ab48ef5e
CryoViking <cryosis@cryodeveloper.xyz>
2026-07-16 01:12:42 +0800

    Some better error handling

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const cli = @import("cli.zig");
 
-pub fn main(init: std.process.Init) !void {
+pub fn main(init: std.process.Init) !u8 {
     // A pre-initialised general purpose allocator for temporary heap work
     const allocator = init.gpa;
 
@@ -15,7 +15,38 @@ pub fn main(init: std.process.Init) !void {
         try list.append(allocator, arg);
     }
 
-    _ = try cli.parse(list.items);
+    if (cli.parse(list.items)) |request| {
+        std.debug.print("command: {s}\n", .{
+            @tagName(request.command),
+        });
 
-    std.debug.print("MusicHook is awake\n", .{});
+        if (request.alias) |alias| {
+            std.debug.print("alias: {s}\n", .{
+                alias,
+            });
+        }
+
+        return 0;
+    } else |err| {
+        switch (err) {
+            cli.ParseError.UnknownCommand => {
+                // TODO: Should get the unknown command and print it somehow
+                std.debug.print("Unknown command given\n", .{});
+            },
+            cli.ParseError.MissingCommand => {
+                // TODO: Should print help menu maybe?
+                std.debug.print("Missing a command argument\n", .{});
+            },
+            cli.ParseError.UnexpectedArgument => {
+                // TODO: Should get number of given arguments
+                // AND number of expected arguments and show something
+                // along those lines
+                std.debug.print("Unexpected argument given\n", .{});
+            },
+            cli.ParseError.MissingAlias => {
+                std.debug.print("No playlist alias given\n", .{});
+            },
+        }
+        return 1; // return a non-zero exit code
+    }
 }
